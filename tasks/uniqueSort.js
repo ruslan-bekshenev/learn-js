117938202ab7b49791dae1813fb44724d1d09ef9
Ruslan <rbekshenev23@gmail.com>
2022-01-22 17:29:42 +0500

    задание из видео https://youtu.be/HCXDrVikPGk?t=2100 refactor

--- tasks/uniqueSort.js
+++ tasks/uniqueSort.js
@@ -3,8 +3,7 @@ let words = ['grapefruit', 'banana','banana', 'grapefruit', 'banana', 'orange', 
 
 function uniqueSort(arr) {
   const uniqueObject = arr.reduce((acc, item) => ({ ...acc, [item]: (acc[item] || 0) + 1 }), {})
-  const sortValues = Object.keys(uniqueObject).sort((a,b) => uniqueObject[b] - uniqueObject[a])
-  return sortValues
+  return Object.keys(uniqueObject).sort((a,b) => uniqueObject[b] - uniqueObject[a])
 }
 
 console.log(uniqueSort(words))
