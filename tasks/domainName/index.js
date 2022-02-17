d890e495ff40a2151d5ef6f3e09f4b1b8261ffa3
Ruslan <rbekshenev23@gmail.com>
2022-02-18 02:48:55 +0500

    domain name

--- tasks/domainName/index.js
+++ tasks/domainName/index.js
@@ -6,3 +6,10 @@ console.log(domainName("http://github.com/carbonfive/raygun"))
 console.log(domainName("http://www.zombie-bites.com"))
 console.log(domainName("https://www.cnet.com"))
 console.log(domainName("www.xakep.ru"))
+
+function domainName(url){
+  url = url.replace("https://", '');
+  url = url.replace("http://", '');
+  url = url.replace("www.", '');
+  return url.split('.')[0];
+};
